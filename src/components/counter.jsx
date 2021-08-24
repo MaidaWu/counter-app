95194913582268db4d1d8a7dc357bf41216028e8
Maida Wu <51833707+Maida-Wu@users.noreply.github.com>
2021-08-24 14:47:24 -0400

    Dynamic rendering

---
+++ src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+}
+
+export default Counter;
