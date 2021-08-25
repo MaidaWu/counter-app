ddbf9941e05efef57882bb4bee8379977f2aa051
Maida Wu <51833707+Maida-Wu@users.noreply.github.com>
2021-08-25 15:52:26 -0400

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,15 +14,15 @@ class Counter extends Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
